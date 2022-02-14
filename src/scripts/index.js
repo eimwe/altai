74eabb5181cab711f2c2b5d6b1463c7544fbed72
eimwe <prokopenkoserge@inbox.ru>
2022-02-14 19:32:35 +0600

    Show/hide function for the mobile navbar has been created

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,23 @@
 import '../styles/style.scss';
+
+/**
+ * @function showMenu
+ * @description shows/hides modal navbar by clicking menu button
+ * @param {undefined}
+ * @returns {undefined}
+ */
+const showMenu = () => {
+  let menuButton = document.getElementById('menu-toggle'),
+      navbarNode = document.querySelector('.navbar');
+
+  menuButton.addEventListener('change', () => {
+
+    navbarNode.classList.add('navbar--opened');
+
+    if(!menuButton.checked) {
+      navbarNode.classList.remove('navbar--opened');
+    }
+  });
+}
+
+showMenu();
