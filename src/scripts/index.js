502bd1af30df0b36881029f637245bb63fc83ed6
eimwe <prokopenkoserge@inbox.ru>
2022-02-27 21:16:40 +0600

    Photoreport gallery has been scripted
    1. Gallery class was imported from './src/scripts' directory;
    2. A new instance of the Gallery class has been created;
    3. All required nodes was acquired and saved to the corresponding variables;
    4. Event listeners and corresponding handlers were attached to the gallery controls.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../styles/style.scss';
-
+import Gallery from './gallery.js';
 /**
  * @function showMenu
  * @description shows/hides modal navbar by clicking menu button
@@ -21,3 +21,32 @@ const showMenu = () => {
 }
 
 showMenu();
+
+/**
+ * Variables for a snapshot gallery
+ * @member {HTMLElement}
+ */
+let snapshotGallery = document.querySelector('.photoreport__snapshots'),
+    snapshotNext = document.querySelector('.gallery--photoreport .gallery__control--next'),
+    snapshotPrev = document.querySelector('.gallery--photoreport .gallery__control--prev');
+
+/**
+ * @constant
+ * @default
+ * @memberof Gallery
+ * @instance
+ */
+const snapshotSlider = new Gallery(snapshotGallery, 9000);
+
+/**
+ * Show next slide
+ * @listens click
+ * @fires next
+ */
+snapshotNext.addEventListener('click', () => snapshotSlider.next());
+/**
+ * Show previous slide
+ * @listens click
+ * @fires prev
+ */
+snapshotPrev.addEventListener('click', () => snapshotSlider.prev());
