1f34e9ac4407bd075a79489e01996ad62b18f778
eimwe <prokopenkoserge@inbox.ru>
2022-03-01 00:07:57 +0600

    An instance of Schedule gallery has been created
    1. Required Schedule gallery nodes have been linked to the appropriate variables;
    2. A new function 'indexGalleries' has been written for indexing both pages and slides to associate them with each other afterwards.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,17 @@
 import '../styles/style.scss';
-import Gallery from './gallery.js';
+import { Gallery, OwlCarousel } from './gallery.js';
+
+/**
+ * @function indexGalleries
+ * @description enumerates slides and pages with the data-id attribute
+ * @param {HTMLCollection} lists - a collection of HTML nodes
+ * @returns {undefined}
+ */
+const indexGalleries = (lists) => {
+  lists = Array.from(lists.children);
+  lists.forEach((listItem, index) => listItem.setAttribute('data-id', `${index}`));
+}
+
 /**
  * @function showMenu
  * @description shows/hides modal navbar by clicking menu button
@@ -50,3 +62,48 @@ snapshotNext.addEventListener('click', () => snapshotSlider.next());
  * @fires prev
  */
 snapshotPrev.addEventListener('click', () => snapshotSlider.prev());
+
+/**
+ * Variables for a schedule gallery
+ * @member {HTMLElement}
+ */
+let scheduleGallery = document.querySelector('.schedule__list'),
+    schedulePages = document.querySelector('.gallery--schedule .gallery__pagination'),
+    scheduleNext = document.querySelector('.gallery--schedule .gallery__control--next'),
+    schedulePrev = document.querySelector('.gallery--schedule .gallery__control--prev');
+
+/**
+ * Enumerated schedule gallery slides
+ */
+indexGalleries(scheduleGallery);
+
+/**
+ * Enumerated schedule gallery pages
+ */
+indexGalleries(schedulePages);
+
+/**
+ * @constant
+ * @default
+ * @memberof OwlCarousel
+ * @instance
+ */
+const scheduleSlider = new OwlCarousel(
+        scheduleGallery,
+        5000,
+        'gallery__slide--active',
+        schedulePages,
+        'gallery__page--active');
+
+/**
+ * Show next schedule gallery slide
+ * @listens click
+ * @fires next
+ */
+scheduleNext.addEventListener('click', () => scheduleSlider.next());
+/**
+ * Show next schedule gallery slide
+ * @listens click
+ * @fires prev
+ */
+schedulePrev.addEventListener('click', () => scheduleSlider.prev());
