801f6420bbe1a8a724225249eda65cfb1fb8e420
eimwe <prokopenkoserge@inbox.ru>
2022-03-14 22:00:56 +0600

    New code for slick slider has been added
    1. Slick slider related code was added to the 'teamGallery.js' file;
    2. Previous test code has been removed from the same file along with test markup in the 'index.html' file.

--- src/scripts/teamGallery.js
+++ src/scripts/teamGallery.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
-  $('.your-class').slick({
+  $('.team__mates').slick({
     dots: true,
+    mobileFirst: true,
+    arrows: false,
     speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          centerMode: true,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false,
+          arrows: true,
+          centerMode: true,
+          slidesToShow: 3
+        }
+      }
+    ]
   });
 });
